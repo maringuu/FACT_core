e8c382f498b64ad5b8e82a12ac1db80002ec38bb
Marten Ringwelski <git@maringuu.de>
2021-06-11 09:47:59 +0200

    file_system_metadata: comment out docker usage

--- src/plugins/analysis/file_system_metadata/install.sh
+++ src/plugins/analysis/file_system_metadata/install.sh
@@ -3,6 +3,6 @@
 # change cwd to current file's directory
 cd "$( dirname "${BASH_SOURCE[0]}" )" || exit 1
 
-docker build -t fs_metadata_mounting docker || exit 1
+#docker build -t fs_metadata_mounting docker || exit 1
 
 exit 0
